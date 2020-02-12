dd5abd8c86ce952b543ae045860056c31ae848d6
Jason Collum <jasoncollum@gmail.com>
2020-02-11 22:12:59 -0600

    querying database for stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-    console.log(req.name)
     res.render('index');
 };
 
@@ -15,3 +14,9 @@ exports.createStore = async (req, res) => {
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+    // Query db for list of all stores
+    const stores = await Store.find();
+    res.render('stores', { title: 'stores', stores });
+}
